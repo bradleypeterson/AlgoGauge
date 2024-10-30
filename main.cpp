588d1f01c9c327be53c0dd079b8e3b383b04ccab
Coleton Watt <electricninja102@gmail.com>
2024-10-29 22:58:13 -0600

    idk perf is weird

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  */
 
 #include "CLI_Parsing/CLI_Parser.hpp"
-
+#include "dependencies/PerfEvent.hpp"
 int main(int argc, char *argv[]) {
     /*
      * Getting Started:
@@ -21,8 +21,9 @@ int main(int argc, char *argv[]) {
      *      ./AlgoGauge --algo quick --len 100000 -r --algo bubble --len 100000 -r
      *      ./AlgoGauge --algo quick --len 10000 --algo bubble --len 10000 --algo selection --len 10000 --algo insertion --len 10000 --algo merge --len 10000 --algo heap --len 10000 --algo quick --len 10000 --algo bubble --len 10000 --algo selection --len 10000 --algo insertion --len 10000 --algo merge --len 10000 --algo heap --len 10000 --algo quick --len 10000 --algo bubble --len 10000 --algo selection --len 10000 --algo insertion --len 10000 --algo merge --len 10000 --algo heap --len 10000 --algo quick --len 10000 --algo bubble --len 10000 --algo selection --len 10000 --algo insertion --len 10000 --algo merge --len 10000 --algo heap --len 10000 --algo quick --len 10000 --algo bubble --len 10000 --algo selection --len 10000 --algo insertion --len 10000 --algo merge --len 10000 --algo heap --len 10000 -rrrrrrssssssooooooeeeeeecccccc
      *      ./AlgoGauge --algo quick --len 1000 --algo bubble --len 1000 --algo selection --len 1000 --algo insertion --len 1000 --algo merge --len 1000 --algo heap --len 1000 -rrrrrr
-     */
-
+     */    
+   
+	PerfEvent e;
     runProgram(argc, argv);
     return 0;  // returning 0 regardless of error
     // as for some reason, node treats a non-0 exit code as a server
